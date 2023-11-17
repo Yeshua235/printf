fd3c414f938ff1098718ef0b200e859d23ddde5f
Yeshua235 <joshuaayanlowo235@gmail.com>
2023-11-16 16:32:30 -0800

    printf project

--- main.h
+++ main.h
@@ -4,6 +4,30 @@
 #include <stdio.h>
 #include <unistd.h>
 
+#define UNUSED(x) (void)(x)
+#define BUFF_SIZE 1024
+
+/**
+* struct fmt - Struct op
+*
+* @fmt: The format.
+* @fn: The function associated.
+*/
+struct fmt
+{
+char fmt;
+int (*fn)(va_list, char[], int, int, int, int);
+};
+
+
+/**
+* typedef struct fmt fmt_t - Struct op
+*
+* @fmt: The format.
+* @fm_t: The function associated.
+*/
+typedef struct fmt fmt_t;
+
 int _printf(const char *format, ...);
 
 
